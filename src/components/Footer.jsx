4f59613c9bf09dd4908d9460b0ea415999d159d9
Fahaths <fahathfahath650@gmail.com>
2025-02-07 11:46:16 +0530

    change the footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
-        <p>&copy; 2023 Measi E-learning. All rights reserved.</p>
+        <p>&copy; 2025 Measi E-learning. All rights reserved.</p>
         <p>
           <a href="/about">About Us</a> | <a href="/contact">Contact</a>
         </p>
